a8218f9cce9086d5dc55098ff52690bc0bc07ab3
Megan Flood <mak.flood@comcast.net>
2017-12-19 13:50:55 -0800

    Completed capture from camera Camera has buttons to capture a single image. Added JS to handle the camera. Added route to save image sent from camera.

--- emotionreader/emotionreader/static/emotionreader/imagecapture.js
+++ emotionreader/emotionreader/static/emotionreader/imagecapture.js
@@ -1,21 +1,46 @@
-var video = document.querySelector('video');
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-var localMediaStream = null;
+'use strict';
 
-function snapshot() {
-  if (localMediaStream) {
-    ctx.drawImage(video, 0, 0);
-    // "image/webp" works in Chrome.
-    // Other browsers will fall back to image/png.
-    document.querySelector('img').src = canvas.toDataURL('image/webp');
-  }
-}
+var app = app || {};
 
-video.addEventListener('click', snapshot, false);
+(function(module) {
 
-// Not showing vendor prefixes or code that works cross-browser.
-navigator.getUserMedia({video: true}, function(stream) {
-  video.src = window.URL.createObjectURL(stream);
-  localMediaStream = stream;
-}, errorCallback);
+    let video = document.querySelector('video');
+    let canvas = document.querySelector('canvas');
+    let ctx = canvas.getContext('2d');
+    let localMediaStream = null;
+
+    function snapshot() {
+      if (localMediaStream) {
+        let image = localMediaStream.getVideoTracks()[0].getSettings();
+        let xRatio = canvas.width / image.width;
+        let yRatio = canvas.height / image.height;
+        let ratio = Math.min(xRatio, yRatio);
+
+        ctx.drawImage(video, 0, 0, image.width, image.height,
+                             0, 0, image.width * ratio, image.height * ratio);
+
+        document.querySelector('img').src = canvas.toDataURL('image/jpeg');
+        video.pause();
+        localMediaStream.getVideoTracks()[0].stop();
+        $('#capture').hide();
+      }
+    }
+
+    function startCamera() {
+        navigator.mediaDevices.getUserMedia({video: true})
+        .then(stream => {
+          video.src = window.URL.createObjectURL(stream);
+          localMediaStream = stream;
+          $('#start-camera').hide();
+          $('#capture').show();
+        })
+        .catch(console.error);
+    }
+
+    app.setupImageCap = function() {
+        $('#capture').hide();
+        $('#start-camera').on('click', startCamera)
+        $('#capture').on('click', snapshot) 
+    }
+
+})(app);
