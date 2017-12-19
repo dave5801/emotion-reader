5341fa5a3624eae249f3fe81fdb9cb25d53780e3
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-19 15:43:33 -0800

    Graph renders with fake data.

--- emotionreader/emotion_emotions/urls.py
+++ emotionreader/emotion_emotions/urls.py
@@ -1,7 +1,9 @@
+"""URLs for emotion app."""
 from django.conf.urls import url
-from emotion_emotions.views import ImageCapView, save_image
+from emotion_emotions.views import ImageCapView, save_image, EmotionAnalysis
 
 urlpatterns = [
     url(r'^imagecap$', ImageCapView.as_view(), name='imagecap'),
-    url(r'^savecap$', save_image, name='savecap')
+    url(r'^savecap$', save_image, name='savecap'),
+    url(r'^analysis$', EmotionAnalysis.as_view(), name='emotion_analusis'),
 ]
--- emotionreader/emotion_emotions/views.py
+++ emotionreader/emotion_emotions/views.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
+from bokeh.plotting import figure
+from bokeh.embed import components
+
 from base64 import b64decode
 import os
 
@@ -27,3 +30,27 @@ def save_image(request):
     with open(os.path.join(settings.MEDIA_ROOT, 'testfile.jpg'), 'wb') as f:
         f.write(image)
     return HttpResponse('Complete')
+
+
+class EmotionAnalysis(TemplateView):
+    """View for emotion analysis."""
+
+    template_name = 'emotion_emotions/emotion_analysis.html'
+
+    def get_context_data(self):
+        """Load a plot to the view."""
+        context = super(EmotionAnalysis, self).get_context_data()
+
+        x = [1, 2, 3, 4, 5]
+        y = [6, 7, 2, 4, 5]
+
+        p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+        p.line(x, y, legend="Temp.", line_width=2)
+
+        script, div = components(p)
+
+        context['plot_html'] = div
+        context['plot_script'] = script
+
+        return context
