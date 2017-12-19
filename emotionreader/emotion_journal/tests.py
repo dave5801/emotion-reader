da458b2e284c5e97b31c11e849879586154df8b1
Michael Shinners <mishinners@icloud.com>
2017-12-19 09:33:25 -0800

    Creates and updates journal test suite.

--- emotionreader/emotion_journal/tests.py
+++ emotionreader/emotion_journal/tests.py
@@ -5,43 +5,9 @@ from django.urls import reverse_lazy
 from emotion_journal.models import Journal
 from emotion_profile.tests import UserFactory
 
-import os
-
 
 class JournalViewUnitTests(TestCase):
     """Tests for the functions of the Journal section of the Emotion app."""
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up a dummy request."""
-        super(JournalViewUnitTests, cls).setUpClass()
-        os.system('mkdir {}'.format(
-            os.path.join(settings.BASE_DIR, 'test_media_for_home')
-        ))
-
-    @classmethod
-    def tearDownClass(cls):
-        """Remove the test directory."""
-        super(JournalViewUnitTests, cls).tearDownClass()
-        os.system('rm -rf {}'.format(
-            os.path.join(settings.BASE_DIR, 'test_media_for_home')
-        ))
-
-
-# @override_settings(MEDIA_ROOT=os.path.join(settings.BASE_DIR,
-#                        'test_media_for_home'))
-#     def test_home_view_returns_context_with_photo_from_db(self):
-#         """Test that the homeview  returns photo from db."""
-#         from emotion_journal.views import JournalView
-#         user = UserFactory()
-#         user.set_password('password')
-#         user.save()
-#         photo = PhotoFactory(user=user, title='test', published='PUBLIC')
-#         photo.save()
-#         view = HomeView()
-#         data = view.get_context_data()
-#         self.assertIn('hero_img_url', data)
-#         self.assertEqual('test', data['hero_img_title'])
 
 
 class MainRoutingTests(TestCase):
@@ -78,7 +44,19 @@ class MainRoutingTests(TestCase):
         response = self.client.get(reverse_lazy('journal'))
         self.assertEqual(response.status_code, 302)
 
+    def test_journal_route_accessible_when_logged_in(self):
+        """Test that a user can only see their journal page if logged in here."""
+        self.client.login(username='mike', password='password')
+        response = self.client.get(reverse_lazy('journal'))
+        self.assertEqual(response.status_code, 200)
+
     def test_create_journal_route_accessible_only_if_logged_in(self):
         """Test that a user can only see their  create journal page if logged in here."""
         response = self.client.get(reverse_lazy('create_journal'))
         self.assertEqual(response.status_code, 302)
+
+    def test_create_journal_route_accessible_when_logged_in(self):
+        """Test that a user can only create a journal entry if logged."""
+        self.client.login(username='mike', password='password')
+        response = self.client.get(reverse_lazy('create_journal'))
+        self.assertEqual(response.status_code, 200)
