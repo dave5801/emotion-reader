9952ac381a2b9d2be7a80495e2dac98f9b2387c1
David Franklin <dave5801@gmail.com>
2017-12-19 10:55:46 -0800

    created face verification object

---
+++ test/face_verification.py
@@ -0,0 +1,50 @@
+"""Class which facial recognition in authenticion."""
+
+'''
+TEST IDEAS:
+test if object created
+test if object created valid
+test if object created invalid
+test if dir is valid
+test if dir contains invalid file
+
+'''
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+class FaceVerificationObject(object):
+    """Face Verification Object."""
+    def __init__(self, filepath_for_faces=None, face_to_verify=None):
+        self.filepath_for_faces = filepath_for_faces
+        self.face_to_verify = face_to_verify
+
+        self.faceID = None
+
+    def get_faces_from_dir(self):
+        """Get Directory of Faces."""
+        if not os.path.isdir(self.filepath_for_faces):
+            return "Invalid File Path"
+
+        return [f for f in listdir(self.filepath_for_faces) if isfile(join(self.filepath_for_faces, f))]
+
+'''
+    def detected(image, face_id=True, landmarks=False, attributes=''):
+        return None
+
+    def verified(face_id, another_face_id=None, person_group_id=None,
+           person_id=None)
+        return None
+
+    def grouped(list_of_face_ids):
+        return None'''
+
+if __name__ == '__main__':
+
+    file_dir = "nicholas_cage"
+
+    fvo = FaceVerificationObject(file_dir)
+
+    list_of_faces = fvo.get_faces_from_dir()
+    print(list_of_faces)
