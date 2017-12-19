3783b8dc44fa5b7afedc101b5b92d961696f39f6
David Franklin <dave5801@gmail.com>
2017-12-18 18:31:37 -0800

    test face is detected

--- test/test_face_verification.py
+++ test/test_face_verification.py
@@ -1,10 +1,14 @@
-#test for dir contents
-#test invalid directory
+"""Test Facial Image Verification."""
 
 #test image is not verified is false, cage and travolta
 #test image is verified, cage and cage
 #test grouping happened, one messy group
 #test grouping, no messy group
+from select_from_face_dir import select_from_face_dir
+FILEPATH = "nicholas_cage"
+TEST_FACE_LIST = select_from_face_dir(FILEPATH)
+
+
 
 def test_select_from_face_dir():
     from select_from_face_dir import select_from_face_dir
@@ -20,3 +24,11 @@ def test_select_from_face_dir_invalid_dir():
     filepath = "thisIsaBadDirectory"
     x = select_from_face_dir(filepath)
     assert x == "Invalid File Path"
+
+
+def test_face_detected():
+    from face_off import detect
+
+    x = detect(FILEPATH + "/" +TEST_FACE_LIST[0])
+    assert x != None
+
