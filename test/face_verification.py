7b757394d307d58f09ea575adc9e16680984fbc7
David Franklin <dave5801@gmail.com>
2017-12-19 11:08:33 -0800

    checked individual face image

--- test/face_verification.py
+++ test/face_verification.py
@@ -13,6 +13,7 @@ test if dir contains invalid file
 import os
 from os import listdir
 from os.path import isfile, join
+from pathlib import Path
 
 class FaceVerificationObject(object):
     """Face Verification Object."""
@@ -28,6 +29,16 @@ class FaceVerificationObject(object):
             return "Invalid File Path"
 
         return [f for f in listdir(self.filepath_for_faces) if isfile(join(self.filepath_for_faces, f))]
+
+    def check_valid_face_file(self, face_file):
+        """Validate an individual face image."""
+
+        face_image_file_path = self.filepath_for_faces + "/" +face_file
+
+        face_image_file = Path(face_image_file_path)
+
+        return face_image_file.exists()
+
 
 '''
     def detected(image, face_id=True, landmarks=False, attributes=''):
@@ -48,3 +59,7 @@ if __name__ == '__main__':
 
     list_of_faces = fvo.get_faces_from_dir()
     print(list_of_faces)
+
+    #valid_face_file = fvo.check_valid_face_file(list_of_faces[0])
+    valid_face_file = fvo.check_valid_face_file("badfile.png")
+    print(valid_face_file) 
