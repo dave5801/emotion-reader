cc69308513d4c80bcd482600d923acadbe01fbae
Michael Shinners <mishinners@icloud.com>
2017-12-20 08:18:48 -0800

    Beginning refactoring of html for new bootstrap.

---
+++ emotion-palette.py
@@ -0,0 +1,26 @@
+"""Set up palette to change color based on defined mood."""
+
+
+mood_palette_dict = {
+    "Anger": "Red",
+    "Contempt": "LtGreen",
+    "Fear": "Purple",
+    "Disgust": "Brown",
+    "Happiness": "Yellow",
+    "Nuetral": "Grey",
+    "Sadness": "Blue",
+    "Surprise": "Orange"
+}
+
+
+def color_theme():
+    """Set the color pallete based on the defined mood."""
+    if mood_palette_dict.key return val
+ { temp } 
+
+
+"""
+Psuedo:
+if key(mood) is the input:
+    choose val(theme class)
+"""
