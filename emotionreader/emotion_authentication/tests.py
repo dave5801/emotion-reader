59d45b2513d39dcacbc80ebcfc86e03daaa3e6d4
David Franklin <dave5801@gmail.com>
2017-12-22 09:15:18 -0800

    cleaned up tests

--- emotionreader/emotion_authentication/tests.py
+++ emotionreader/emotion_authentication/tests.py
@@ -1,30 +1,22 @@
 """Test Facial Image Verification."""
-#./manage.py test emotion_authentication.tests
+
 from django.test import TestCase
-from django.test import TestCase, RequestFactory
-from emotion_authentication.models import FaceVerificationManager, create_face_verification_object
+from django.test import RequestFactory
+from emotion_authentication.models import FaceVerificationManager
 from django.conf import settings
 from emotion_authentication.face_verification import FaceVerification
 import os
 from emotion_profile.tests import UserFactory
 import factory
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import Client
 from django.utils import timezone
 from emotion_profile.models import User
 from django.core.urlresolvers import reverse_lazy
 from unittest.mock import patch
 
 filepath = "nicholas_cage"
-full_path = os.path.join(settings.BASE_DIR, 'emotion_authentication/' + filepath)
+full_path = os.path.join(settings.BASE_DIR,'emotion_authentication/' + filepath)
 
-'''
-TEST_REG_PHOTO = SimpleUploadedFile(name='cage1', 
-    content=open(os.path.join(full_path,'cage1.png'), 'rb').read(), content_type='image/png')
-
-TEST_NEW_PHOTO = SimpleUploadedFile(name='cage2',
- content=open(os.path.join(full_path,'cage2.png'), 'rb').read(), content_type='image/png')
-'''
 
 class FaceDetectionTests(TestCase):
     """Test Basic Face Detection."""
@@ -48,7 +40,6 @@ class FaceDetectionTests(TestCase):
 
     def test_face_detected(self):
         """Test face is detected."""
-
         patcher = patch('cognitive_face.util.request', return_value=[{'faceId': 'xxxxx'}])
         patcher.start()
         self.addCleanup(patcher.stop)
@@ -67,7 +58,6 @@ class FaceDetectionTests(TestCase):
 
 class FaceVerificationTests(TestCase):
     """Test if User can be authenticated."""
-        
     def setUp(self):
         user = UserFactory.create()
         user.set_password(factory.Faker('password'))
@@ -82,19 +72,14 @@ class FaceVerificationTests(TestCase):
 
     def test_new_user_face_verification_return_no_reg_photo(self):
         """test if fvo invalid."""
-        #self.assertIsNone(self.user.faces.auth_face)
         self.assertIsNone(self.user.faces.auth_face_id)
         self.assertIsNone(self.user.faces.auth_last_recorded)
 
-        ''''''
+        
     def test_new_user_set_face_image_for_auth_return_valid(self):
-
-        filepath = "nicholas_cage"
+        '''Test for valid object returns.'''
+        filepath = "nicholas_cage" #This is a test directory with gag photos.
         full_path = os.path.join(settings.BASE_DIR, 'emotion_authentication/' + filepath)
-
-        verify = FaceVerification(filepath_for_faces=full_path)
-        #x = verify.detected('cage1.png')
-        request = self.request.get('')
 
         test_face_verifier = FaceVerificationManager.objects.get(user=self.user)
         with open(os.path.join(full_path, 'cage1.png'), 'rb') as f:
@@ -113,9 +98,10 @@ class FaceVerificationTests(TestCase):
 
 
 class FaceVerificationViewTests(TestCase):
-    """Tests if authentication pages work properly."""
+    """Tests if authentication pages route properly."""
 
     def setUp(self):
+        """"user and mock api calls."""
         user = UserFactory.create()
         user.set_password(factory.Faker('password'))
         user.save()
@@ -123,17 +109,11 @@ class FaceVerificationViewTests(TestCase):
         patcher = patch('cognitive_face.util.request', return_value={0: {'faceId': 'xxxxx'}, 'isIdentical': True})
         patcher.start()
         self.addCleanup(patcher.stop)
-        '''
-        def test_record_emotions_route_post_has_302_not_logged_in(self):
-        """Test that record emotions redirects when not logged in."""
-        response = self.client.post(reverse_lazy('record_emotions'))
-        self.assertEqual(response.status_code, 302)
-        '''
-        #test if registration routes to face auth
-    def test_route_to_face_auth(self):
 
+    def test_route_to_face_auth(self):
+        """test if registration routes to face auth."""
         with open(os.path.join(full_path, 'cage1.png'), 'rb') as f:
-            content = f.read()
+            content = f.read()#create file from file path
 
         self.user.faces.auth_face = SimpleUploadedFile(name="cage1",
                                                      content=content,
@@ -142,7 +122,3 @@ class FaceVerificationViewTests(TestCase):
         data = {'image': 'data: base64,FAKE'}
         response = self.client.post(reverse_lazy('face_verification'), data)
         self.assertEqual(response.status_code, 302)
-
-
-        #test if registration view routes to verification view
-
