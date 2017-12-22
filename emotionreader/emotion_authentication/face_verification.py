932fcc93c54c3e50b417bc0897184532008f753a
David Franklin <dave5801@gmail.com>
2017-12-22 09:30:26 -0800

    doc strings to face verification.py, cleaned up code

--- emotionreader/emotion_authentication/face_verification.py
+++ emotionreader/emotion_authentication/face_verification.py
@@ -1,4 +1,10 @@
-"""Access the Face API."""
+"""
+Face Verification Object
+
+This checks if an individual is recognized by the system
+This object is used during the authentication phase.
+"""
+
 import os
 from os import listdir
 from os.path import isfile, join
@@ -9,10 +15,10 @@ from django.utils import timezone
 
 
 class FaceVerification(object):
-    """Face Verification Object."""
+    """Create Class."""
 
     def __init__(self, filepath_for_faces=None, face_to_verify=None):
-        """Init the class."""
+        """Initialization."""
         Key.set(os.environ.get('FACE_API_KEY1', ''))
         BaseUrl.set(os.environ.get('FACE_URL', ''))
 
@@ -34,15 +40,12 @@ class FaceVerification(object):
 
     def check_valid_face_file(self, face_file):
         """Validate an individual face image."""
-        print(face_file)
         face_image_file = Path(face_file)
 
         return face_image_file.exists()
 
-
     def detected(self, image=None, face_id=True, landmarks=False, attributes='', img_stream=False):
         """Returns the ID of a detected face."""
-
         if not image:
             return False
 
@@ -71,18 +74,17 @@ class FaceVerification(object):
         detection = CF.util.request(
             'POST', url, headers=headers, params=params, json=json, data=data)
 
-        # NOTE - this is how you get valid faceIDs --> detection[0]['faceId']
-
         return detection
 
-    '''Note: Not used currently, will be at later time.'''
+    '''NOTE: This is not in use currently, but will be useful later.'''
     def group_verify(self, face_id, list_of_face_ids):
-        """Any face image found in 'messy groud' is not verified."""
+        """Find individual by comparing across groups."""
         url = 'group'
         json = {
             'faceIds': list_of_face_ids,
         }
 
+        """Any face image found in 'messy groud' is not verified."""
         grouping = CF.util.request('POST', url, json=json)
         primary_group = grouping['groups'][0]
         messy_group = grouping['messyGroup']
@@ -134,54 +136,17 @@ class FaceVerification(object):
 
         registration_verification = CF.util.request('POST', url, json=json)
 
-        print(registration_verification)
-
         return registration_verification['isIdentical']
 
 
 if __name__ == '__main__':  # pragma: nocover
+    """Basic Usage of Face Verification."""
 
     file_dir = "nicholas_cage"
 
     fvo = FaceVerification(file_dir)
 
     list_of_faces = fvo.get_faces_from_dir()
-    #print(list_of_faces)
 
-    ''''''
-    #valid_face_file = fvo.check_valid_face_file("cage1.png")
-    #print(valid_face_file)
     valid_face_file = fvo.detected("cage1.png")[0]['faceId']
     print(valid_face_file)
-
-    '''detected_faces = []
-
-    for i in list_of_faces:
-        detected_faces.append(fvo.detected(i))
-
-    print(detected_faces)'''
-
-    #x = fvo.group_verify(detected_faces)
-    #print("GROUPS", x['groups'][0])
-    #print("MESSY GROUP", x['messyGroup'][0])
-
-    #x = fvo.group_verify(detected_faces[0], detected_faces)
-    #print(x)
-
-    #registration_photo_id = fvo.detected("cage1.png")
-    #print(registration_photo_id)
-
-    #x = fvo.verify_against_registration(detected_faces[0])
-    #travolta = fvo.detected("travolta1.png")
-    #x = fvo.verify_against_registration(travolta)
-    #print(x)
-
-    #img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-    #img_url = 'not_face.png'
-    #single_face = fvo.verifiy_new_user_face(img_url)
-    #print(single_face)
-
-    #face2 = "cage1.png"
-    #face1 = fvo.detected("cage_and_other_person.png")
-    #x = fvo.verify_against_registration(face1)
-    #print(x)
